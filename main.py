ef57c45c472f3062fb3f0c489075db6b403a42ea
Anastasya Zolotukhina <anastasiazolotuhina17@gmail.com>
2022-02-14 22:41:43 +0300

    add required number of lines

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore, QtGui
+
 
 from MainWindow import Ui_cake_calc
 
@@ -9,6 +10,44 @@ class MainWindow(QtWidgets.QMainWindow, Ui_cake_calc):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.btnClicked)
+        self.pushButton_3.clicked.connect(self.setIngreds)
+
+    def setIngreds(self):
+        rows_number = self.spinBox.value()
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.layoutWidget)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.update()
+        self.show()
+#        self.gridLayout = QtWidgets.QGridLayout()
+#        self.gridLayout.setObjectName("gridLayout")
+#        self.label_5 = QtWidgets.QLabel(self.layoutWidget)
+#        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+#        self.label_5.setObjectName("label_5")
+#        self.gridLayout.addWidget(self.label_5, 0, 1, 1, 1)
+#        self.label_6 = QtWidgets.QLabel(self.layoutWidget)
+#        self.label_6.setAlignment(QtCore.Qt.AlignCenter)
+#        self.label_6.setObjectName("label_6")
+#        self.gridLayout.addWidget(self.label_6, 0, 2, 1, 1)
+#        self.label_7 = QtWidgets.QLabel(self.layoutWidget)
+#        self.label_7.setAlignment(QtCore.Qt.AlignCenter)
+#        self.label_7.setObjectName("label_7")
+#        self.gridLayout.addWidget(self.label_7, 0, 3, 1, 1)
+
+#        for i in range(1,rows_number):
+#            self.label_2 = QtWidgets.QLabel(self.layoutWidget)
+#            self.label_2.setObjectName("label_2")
+#            self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
+#            self.lineEdit_1 = QtWidgets.QLineEdit(self.layoutWidget)
+#            self.lineEdit_1.setObjectName("lineEdit_1")
+#            self.gridLayout.addWidget(self.lineEdit_1, 1, 1, 1, 1)
+#            self.lineEdit_2 = QtWidgets.QLineEdit(self.layoutWidget)
+#            self.lineEdit_2.setObjectName("lineEdit_2")
+#            self.gridLayout.addWidget(self.lineEdit_2, 1, 2, 1, 1)
+#            self.lineEdit_3 = QtWidgets.QLineEdit(self.layoutWidget)
+#            self.lineEdit_3.setObjectName("lineEdit_3")
+#            self.gridLayout.addWidget(self.lineEdit_3, 1, 3, 1, 1)
+        self.textEdit.setText(str(rows_number))
+
 
     def btnClicked(self):
         used_1 = float(self.lineEdit_1.text())
